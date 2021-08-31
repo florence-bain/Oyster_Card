7a775946b1a77c4fe181371ecd49fe77954c8d18
Florence Bain <florence_bain@outlook.com>
2021-08-31 16:44:01 +0100

    created a top up method

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -1,10 +1,14 @@
 class Oystercard
+
+  attr_reader :balance 
+
   def initialize
     @balance = 0
   end
-
-  def balance
-    @balance
-  end
+  
+  def top_up(cash)
+    raise 'maximum exceeded' if @balance + cash >= 90
+    @balance += cash
+  end 
 
 end
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -4,4 +4,9 @@ describe Oystercard do
   it 'has a balance of 0 when initialised' do
     expect(subject.balance).to eq 0
   end
+
+  it 'add money to card' do
+    subject.top_up(1)
+    expect(subject.balance).to eq 1
+  end
 end 
